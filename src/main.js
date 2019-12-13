48544ff61b09b8639296daa557a7cfa7b218a329
NoobishNerd <shenitzleweasel@gmail.com>
2019-12-13 18:16:33 +0000

    base componente logins

--- src/main.js
+++ src/main.js
@@ -2,13 +2,21 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-Vue.config.productionTip = false;
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Axios from 'axios';
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount("#app");
+
+
+Vue.prototype.$http = Axios;
+const token = localStorage.getItem('token')
+if (token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+
+Vue.config.productionTip = false;
+}
