ad478c1204c6d570d8e6aa3047aa3a56835a8afe
Figurinha20 <figurinhapt@gmail.com>
2019-12-26 21:37:45 +0000

    Perfil Bety

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,62 @@
 <template>
-  <div class="about">
-    <h1>BURIBANKA BURIBANKA BURIBANKA</h1>
+  <div class="container">
+    <div class="row d-flex justify-content-sm-center">
+      <div class="col-sm-12">
+        <div class="row" style="border:1px solid black;">
+          <div class="col-sm-3 text-center">
+            <br>
+            <br>
+            <img src="../assets/Hachiman Profile Pic 1.jpg" class="rounded-circle" width="95px" height="95px">
+            <br>
+            <h5 class="pt-2">Rui Gonçalves</h5>
+            <button class="mt-2">Alterar Foto de Perfil</button>
+          </div>
+          <div class="col-sm-9">
+            <div class="row justify-content-sm-around">
+              <div class="col-sm-4 py-3" style="border-bottom:1px solid black; border-left:1px solid black;">
+                <h5> Informações Pessoais </h5>
+              </div>
+              <div class="col-sm-4 py-3" style="border-bottom:1px solid black; border-left:1px solid black;">
+                <h5> As Minhas Reservas </h5>
+              </div>
+              <div class="col-sm-4 py-3" style="border-bottom:1px solid black; border-left:1px solid black;">
+                <h5> Terminar Sessão </h5>
+              </div>
+            </div>
+            <br>
+            <br>
+            <div class="row">
+              <div class="col-sm-1">
+                <h5>Nome</h5>
+                <input type="text" placeholder="Rui Gonçalves">
+              </div>
+            </div>
+            <br>
+            <div class="row">
+              <div class="col-sm-1">
+                <h5>Telemóvel</h5>
+                <input type="text" placeholder="914206969">
+              </div>
+            </div>
+            <br>
+            <div class="row">
+              <div class="col-sm-1">
+                <h5>Email</h5>
+                <input type="text" placeholder="querofalecer@gmail.com">
+              </div>
+            </div>
+            <br>
+            <div class="row">
+              <div class="col-sm-8">
+              </div>
+              <div class="col-sm-4">
+                <button>Guardar Alterações</button>
+              </div>
+            </div>
+            <br>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
