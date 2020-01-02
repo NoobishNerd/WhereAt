7b394a49cb6612da0f8f7bc290c5522484154f20
Figurinha20 <figurinhapt@gmail.com>
2020-01-02 17:11:04 +0000

    Style Scoped so It doesnt ruin the place

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -56,7 +56,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 body {
   overflow-y: hidden;
 }
--- src/components/LoginRestaurant.vue
+++ src/components/LoginRestaurant.vue
@@ -76,7 +76,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 body {
   overflow-y: hidden;
 }
