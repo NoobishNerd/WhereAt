12bd1bced78a67af09c92a7aac20685211e1fa91
Figurinha20 <figurinhapt@gmail.com>
2020-01-07 16:51:53 +0000

    Melhorias na profile (hey, that's pretty good)

--- src/views/ClientProfile.vue
+++ src/views/ClientProfile.vue
@@ -1,41 +1,36 @@
 <template>
   <div class="container">
+    <br>
+    <br>
     <div class="row d-flex justify-content-sm-center">
       <div class="col-sm-12">
-        <div class="row" style="border:1px solid black;">
+        <div class="row" style="border:1px solid black; border-radius: 6px;">
           <div class="col-sm-3 text-center">
             <br />
             <br />
-            <img
-              src="../assets/Hachiman Profile Pic 1.jpg"
-              class="rounded-circle"
-              width="95px"
-              height="95px"
-            />
+            <img src="../assets/Hachiman Profile Pic 1.jpg" class="rounded-circle" width="95px" height="95px" />
             <br />
-            <h5 class="pt-2">Rui Gonçalves</h5>
+            <h5 class="pt-2" id="brownBoldText">Rui Gonçalves</h5>
             <button class="mt-2">Alterar Foto de Perfil</button>
           </div>
 
           <div class="col-sm-9">
             <div class="row justify-content-sm-around">
-              <div
-                class="col-sm-4 py-3"
-                style="border-bottom:1px solid black; border-left:1px solid black;"
-              >
-                <h5 @click="call('info')">Informações Pessoais</h5>
+              <div @click="call('info')" class="col-sm-4 py-3" style="border-bottom:1px solid black; border-left:1px solid black; border-radius: 0px 0px 0px 6px; cursor:pointer;">
+                <div class="row">
+                  <div class="col-sm-2">
+                    <img src="../assets/main_user.png" width="45px"> 
+                  </div>
+                  <div class="col-sm-10">
+                    <h5 id="brownText">Informações Pessoais</h5>                    
+                  </div>
+                </div>
               </div>
-              <div
-                class="col-sm-4 py-3"
-                style="border-bottom:1px solid black; border-left:1px solid black;"
-              >
-                <h5 @click="call('history')">As Minhas Reservas</h5>
+              <div @click="call('history')" class="col-sm-4 py-3" style="border-bottom:1px solid black; border-left:1px solid black; cursor: pointer;">
+                <h5 id="brownText">As Minhas Reservas</h5>
               </div>
-              <div
-                class="col-sm-4 py-3"
-                style="border-bottom:1px solid black; border-left:1px solid black;"
-              >
-                <h5>Terminar Sessão</h5>
+              <div @click="logout()" class="col-sm-4 py-3" style="border-bottom:1px solid black; border-left:1px solid black; cursor: pointer;">
+                <h5 id="brownText">Terminar Sessão</h5>
               </div>
             </div>
             <ClientInfo v-if="component == 'info'"> </ClientInfo>
@@ -48,7 +43,7 @@
 </template>
 <script>
 import ClientInfo from "@/components/ClientInfo.vue";
-import ClientHistory from "@/components/ClientInfo.vue";
+import ClientHistory from "@/components/ClientHistory.vue";
 export default {
   data: () => ({
     component: "info"
@@ -57,6 +52,11 @@ export default {
   methods: {
     call(newComponent) {
       this.component = newComponent;
+    },
+    logout() {
+      this.$store.commit("LOGOUT")
+      localStorage.setItem("loggedUser", "")
+      this.$router.replace("/")
     }
   },
   components: {
@@ -65,3 +65,16 @@ export default {
   }
 };
 </script>
+
+<style>
+#brownBoldText{
+  color: #966227;
+  font-family: "Raleway";
+  font-weight: bold;
+}
+
+#brownText{
+  color: #966227;
+  font-family: "Raleway";
+}
+</style>
