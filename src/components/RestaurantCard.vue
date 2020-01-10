93da92ca86be937c54dacac57e1d261bbcc57521
ZeBitzz <9180270@esmad.ipp.pt>
2020-01-10 16:05:09 +0000

    Cartas fresquinhas parecem crianças

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -1,22 +1,35 @@
 <template>
-  <div class="card mb-3">
-    <h5>{{ restaurant.username }}</h5>
-
-    
-    <img :src="restaurant.profilePic" @click="routerLink"/>
-    
-
-    <h3>{{ restaurant.local }}</h3>
-    <h1>{{ restaurant.info }}</h1>
-
+  <div class="card-group">
+    <div class="container-fluid">
+      <div class="card">
+        <img
+          class="card-img-top img-fluid"
+          src="../assets/banana.jpg"
+          alt="Card image cap"
+        />
+        <div class="card-body">
+          <h6 class="card-title text-left">Cozinha Gourmet</h6>
+          <h4 class="card-title text-left font-weight-bold">
+            Screaming Bananas
+          </h4>
+          <p class="card-text text-left">
+            <small>
+              Preço médio:25€
+            </small>
+          </p>
+          <p class="card-text text-left">
+            <small>4.5-4012 avaliações</small>
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
-  
 </template>
 
 <script>
 export default {
   name: "RestaurantCard",
-  
+
   props: {
     restaurant: {
       type: Object,
@@ -26,7 +39,10 @@ export default {
   //COMO FAÇO ISTO EM ROUTER-LINK TO=""???
   methods: {
     routerLink() {
-      this.$router.push({ name: 'aboutRestaurant', params: { id: this.restaurant.id } })
+      this.$router.push({
+        name: "aboutRestaurant",
+        params: { id: this.restaurant.id }
+      });
     }
   }
 };
@@ -34,18 +50,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.card-group {
+  padding-bottom: 40px;
+  font-family: "Raleway";
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.card-body {
+  padding: 0px;
 }
 </style>
