abe49af5a9c82c6a45447eb764d7dfffac578b0d
NoobishNerd <shenitzleweasel@gmail.com>
2020-01-14 16:16:48 +0000

    protótipo para componente comments

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -1,5 +1,25 @@
 <template>
-    
+    <div>
+      <div class="row">
+        <div class="col-sm-2">
+          
+        </div>
+        <div class="col-sm-2">
+          <img :src="comment.profilePic" class="rounded-circle" width="95px" height="95px" />
+          <br />
+          <h5 class="pt-2" id="brownBoldText">{{comment.username}}</h5>
+        </div>
+        <div class="col-sm-6">
+          <h6>{{comment.rate}}</h6>
+          <br>
+          <h5>{{comment.text}}</h5>
+        </div>
+        <div class="col-sm-2">
+          
+        </div>
+      </div>
+
+    </div>
 </template>
 
 <script>
