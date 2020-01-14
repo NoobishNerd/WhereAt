355fc28a7a9845ebcb0f90381d2e5ace19257333
ZeBitzz <9180270@esmad.ipp.pt>
2020-01-14 16:23:53 +0000

    cenas para o luis injetar

--- src/views/AboutRestaurant.vue
+++ src/views/AboutRestaurant.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container border border-light rounded">
+  <div class="container">
     <div class="row">
       <div id="windowPhoto" class="col-sm-5 text-center img-thumbnail">
         <h5 class="pt-2 font-weight-bold"></h5>
@@ -9,7 +9,7 @@
           class="pb-2 img-fluid"
         />
       </div>
-      <div class="col-1"></div>
+      <div class="col-sm/1"></div>
       <div
         id="windowCarrousel"
         class="col-sm-6 text-center img-thumbnail img-fluid"
@@ -102,43 +102,50 @@
       </div>
       <div class="col-sm-1"></div>
       <div class="col-sm-5 ">
-        <img src="../assets/ze.png" alt="" />
+        <div>
+          <div class="google-map img-fluid" id="myMap"></div>
+        </div>
       </div>
     </div>
     <br />
 
-    <div id="finalCrate" class="row d-flex" style="border:1px solid black;">
-      <div class="col-sm-3 pt-3" style="border-bottom:1px solid black;">
-        <h5 class="font-weight-bold">Ementa</h5>
+    <div class="container-fluid">
+      <div id="finalCrate" class="row d-flex">
+        <div id="menu" class="col-sm-3 pt-3" style="border-bottom-lg light:1px">
+          <h5 class="font-weight-bold">Ementa</h5>
+        </div>
+        <div
+          id="promotion"
+          class="col-sm-3 pt-3"
+          style="border-bottom-lg light:1px solid black; border-left-lg light:1px"
+        >
+          <h5 class="font-weight-bold">Promoções</h5>
+        </div>
+        <div
+          id="comentary"
+          class="col-sm-3 pt-3 font-weight-bold "
+          style="border-bottom-lg light:1px; border-left-lg light:1px;"
+        >
+          <h5 class="font-weight-bold">Comentários</h5>
+        </div>
+        <div class="col-sm-1" style="border-left-lg light:1px"></div>
+        <div
+          id="information"
+          class="col-sm-2 pt-2 "
+          style="border-bottom-lg light:1px; border-left-lg light:3px;"
+        >
+          <h1 class="font-weight-bold">i</h1>
+        </div>
+        <p class="text-justify pt-2 ml-3 mr-3">
+          Ementa do Restaurante: Recomendações Do Chef: Bacalhau Lascado
+          Especial – prato de eleição dos clientes da casa 15,5 € um delicioso
+          bacalhau assado na brasa com batatas assadas “a murro” Bacalhau à Brás
+          13,8 € um clássico na gastronomia portuguesa confeccionado com
+          bacalhau de alta qualidade, acompanha com salada Pataniscas de
+          Bacalhau 14,2 € cozinhadas com saborosas lascas de bacalhau, servidas
+          com arroz de feijão ou arroz de tomate Preço médio 25 €
+        </p>
       </div>
-      <div
-        class="col-sm-3 pt-3"
-        style="border-bottom:1px solid black; border-left:1px solid black;"
-      >
-        <h5 class="font-weight-bold">Promoções</h5>
-      </div>
-      <div
-        id="comentary"
-        class="col-sm-3 pt-3 font-weight-bold "
-        style="border-bottom:1px solid black; border-left:1px solid black;"
-      >
-        <h5 class="font-weight-bold">Comentários</h5>
-      </div>
-      <div class="col-sm-1" style="border-left:1px solid black;"></div>
-      <div
-        id="information"
-        class="col-sm-2 pt-2 "
-        style="border-bottom:1px solid black; border-left:1px solid black;"
-      >
-        <h1 class="font-weight-bold">i</h1>
-      </div>
-      Ementa do Restaurante: Recomendações Do Chef: Bacalhau Lascado Especial –
-      prato de eleição dos clientes da casa 15,5 € um delicioso bacalhau assado
-      na brasa com batatas assadas “a murro” Bacalhau à Brás 13,8 € um clássico
-      na gastronomia portuguesa confeccionado com bacalhau de alta qualidade,
-      acompanha com salada Pataniscas de Bacalhau 14,2 € cozinhadas com
-      saborosas lascas de bacalhau, servidas com arroz de feijão ou arroz de
-      tomate Preço médio 25 €
     </div>
   </div>
 </template>
@@ -149,8 +156,12 @@ export default {
     component: "info",
     restaurant: {
       username: "jk"
-    }
+    },
+    map: ""
   }),
+  mounted: function() {
+    this.renderMap();
+  },
 
   created: function() {
     this.restaurant = this.$store.getters.getRestaurantById(
@@ -161,6 +172,13 @@ export default {
   methods: {
     call(newComponent) {
       this.component = newComponent;
+    },
+    renderMap() {
+      this.map = new google.maps.Map(document.querySelector("#myMap"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+      });
+      this.map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
     }
   }
   /*
@@ -181,12 +199,12 @@ img {
 }
 
 html * {
-  color: #966227;
+  color: #f17526;
 }
 #smallerButton {
-  background-color: #e3b814;
+  background-color: #f17526;
   border: none;
-  color: #af2831;
+  color: #ffffff;
   padding: 10px 45px;
   text-align: center;
   text-decoration: none;
@@ -203,51 +221,63 @@ html * {
   -o-transition: all 0.3s ease-in-out;
   transition: all 0.3s ease-in-out;
 }
-#mapsButton {
-  background-color: #e3b814;
-  border: none;
-  color: #af2831;
-  padding: 15px 80px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  text-transform: uppercase;
-  font-size: 20px;
-  font-weight: bold;
-  -webkit-border-radius: 5px 5px 5px 5px;
-  border-radius: 5px 5px 5px 5px;
-  margin: 30px 20px 40px 20px;
-  -webkit-transition: all 0.3s ease-in-out;
-  -moz-transition: all 0.3s ease-in-out;
-  -ms-transition: all 0.3s ease-in-out;
-  -o-transition: all 0.3s ease-in-out;
-  transition: all 0.3s ease-in-out;
-}
 #fotoRestaurante {
   width: 400px;
   border-color: white;
   height: 300px;
 }
 #windowPhoto {
-  border-color: white;
-  box-shadow: 0 30px 60px 15px rgba(0, 0, 0, 0.1);
+  -webkit-box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.75);
 }
 #windowCarrousel {
-  box-shadow: 0 30px 60px 15px rgba(0, 0, 0, 0.1);
+  -webkit-box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.75);
 }
+
 #windowReservation {
   border-color: white;
   box-shadow: 0 30px 60px 15px rgba(0, 0, 0, 0.1);
 }
 #finalCrate {
-  border-radius: 10px 10px 0px 0px;
+  -webkit-box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.75);
+  border: none;
 }
-.container {
+.border-primary {
+  border-width: 3px;
+}
+#promotion {
+  -webkit-box-shadow: 1px 3px 0px 0px rgba(0, 0, 0, 0.25);
+  -moz-box-shadow: 1px 2px 0px 0px rgba(0, 0, 0, 0.25);
+  box-shadow: 1px 2px 0px 0px rgba(0, 0, 0, 0.25);
+}
+#menu {
+  -webkit-box-shadow: 1px 2px 0px 0px rgba(0, 0, 0, 0.25);
+  -moz-box-shadow: 1px 2px 0px 0px rgba(0, 0, 0, 0.25);
+  box-shadow: 1px 2px 0px 0px rgba(0, 0, 0, 0.25);
+}
+#comentary {
+  -webkit-box-shadow: 1px 3px 0px 0px rgba(0, 0, 0, 0.25);
+  -moz-box-shadow: 1px 2px 0px 0px rgba(0, 0, 0, 0.25);
+  box-shadow: 1px 2px 0px 0px rgba(0, 0, 0, 0.25);
+}
+#information {
+  -webkit-box-shadow: -1px 3px 0px 0px rgba(0, 0, 0, 0.25);
+  -moz-box-shadow: -1px 2px 0px 0px rgba(0, 0, 0, 0.25);
+  box-shadow: -1px 2px 0px 0px rgba(0, 0, 0, 0.25);
+}
+#windowEditor {
   -webkit-box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.75);
   -moz-box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.75);
   box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.75);
 }
-.border-primary {
-  border-width: 3px;
+#myMap {
+  -webkit-box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.75);
 }
 </style>
