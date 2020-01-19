5a406def69bcc60c583a5d8066e5d37b6fcc7660
Figurinha20 <figurinhapt@gmail.com>
2020-01-19 16:56:50 +0000

    Saga InfoEditor quase completa, vou só tipo jogar estudar barcos um bocado que estou com tonturas

--- src/components/InfoEditor.vue
+++ src/components/InfoEditor.vue
@@ -1,5 +1,70 @@
 <template>
-    <div>
-        <p>hi</p>
+<div class="container">
+    <div class="form-group">
+        <div class="row">
+            <div class="col-sm-10">
+                <label class="mt-2" for="exampleFormControlTextarea1">Editar Informação</label>
+                <textarea v-model="restaurant.info" class="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>
+            </div>
+            <div class="col-sm-2 align-self-center">
+                <button @click="saveChanges" id="saveChangesBtn" class="py-4 px-2 mt-5 mr-3">Salvar Mudanças</button>
+            </div>   
+        </div>
+
+        <br>
+
+        <div class="row">
+            <div class="col-sm-12">
+                <h4>Informações relevantes</h4>
+                <hr>
+                <h6 v-if="restaurant.info == ''">Este restaurante ainda não adicionou informações adicionais</h6>
+                <h6 v-else>{{restaurant.info}}</h6>
+            </div>
+        </div>
     </div>
+</div>
 </template>
+
+<script>
+export default {
+  name: "InfoEditor",
+  data: () => ({
+
+  }),
+  props:{
+    restaurant:{
+      type: Object,
+      required: true
+    }
+  },
+
+  methods: {
+      saveChanges() {
+          //Isto já muda no state só quero pôr o state na local storage
+          alert("Work in Progress")
+      }
+  }
+
+}
+</script>
+
+<style scoped>
+    #saveChangesBtn {
+        background-color: #f17526;
+        border: none;
+        color: #ffffff;
+        text-align: center;
+        text-decoration: none;
+        text-transform: uppercase;
+        font-size: 15px;
+        font-weight: bold;
+        -webkit-border-radius: 5px 5px 5px 5px;
+        border-radius: 5px 5px 5px 5px;
+        -webkit-transition: all 0.3s ease-in-out;
+        -moz-transition: all 0.3s ease-in-out;
+        -ms-transition: all 0.3s ease-in-out;
+        -o-transition: all 0.3s ease-in-out;
+        transition: all 0.3s ease-in-out;
+    }
+
+</style>
