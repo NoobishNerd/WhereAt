b901ce11b192a89e6e2d23a222fbfd68f3821702
Figurinha20 <figurinhapt@gmail.com>
2020-01-21 21:36:37 +0000

    Rating Stars Working as Intended

--- src/components/AddComment.vue
+++ src/components/AddComment.vue
@@ -9,20 +9,56 @@
             </div>
             <div class="col-sm-11 pl-0">
                 <div class="mt-2 form-group">
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="2"></textarea>
+                    <textarea v-model="newComment" class="form-control" id="exampleFormControlTextarea1"
+                        rows="2"></textarea>
                 </div>
-            </div>        
+            </div>
         </div>
         <div class="row">
             <div class="col-sm-1 pr-0">
             </div>
-            <div class="col-sm-9 pl-0">
-                <img src="../assets/Star.png" width="30px">
-                <img src="../assets/Star.png" width="30px">
-                <img src="../assets/Star.png" width="30px">
-                <img src="../assets/Star.png" width="30px">
-                <img src="../assets/Star.png" width="30px">
-            </div>
+                <div v-if="newRating == ''" class="col-sm-9 pl-0" style="cursor: pointer">
+                    <img @click="ratingStar(1)" src="../assets/Star.png" width="30px">
+                    <img @click="ratingStar(2)" src="../assets/Star.png" width="30px">
+                    <img @click="ratingStar(3)" src="../assets/Star.png" width="30px">
+                    <img @click="ratingStar(4)" src="../assets/Star.png" width="30px">
+                    <img @click="ratingStar(5)" src="../assets/Star.png" width="30px">
+                </div>
+                <div v-if="newRating == 1" class="col-sm-9 pl-0" style="cursor: pointer">
+                    <img @click="ratingStar(1)" src="../assets/Star Colored.png" width="30px">
+                    <img @click="ratingStar(2)" src="../assets/Star.png" width="30px">
+                    <img @click="ratingStar(3)" src="../assets/Star.png" width="30px">
+                    <img @click="ratingStar(4)" src="../assets/Star.png" width="30px">
+                    <img @click="ratingStar(5)" src="../assets/Star.png" width="30px">
+                </div>
+                <div v-if="newRating == 2" class="col-sm-9 pl-0" style="cursor: pointer" >
+                    <img @click="ratingStar(1)" src="../assets/Star Colored.png" width="30px">
+                    <img @click="ratingStar(2)" src="../assets/Star Colored.png" width="30px">
+                    <img @click="ratingStar(3)" src="../assets/Star.png" width="30px">
+                    <img @click="ratingStar(4)" src="../assets/Star.png" width="30px">
+                    <img @click="ratingStar(5)" src="../assets/Star.png" width="30px">
+                </div>
+                <div v-if="newRating == 3" class="col-sm-9 pl-0" style="cursor: pointer">
+                    <img @click="ratingStar(1)" src="../assets/Star Colored.png" width="30px">
+                    <img @click="ratingStar(2)" src="../assets/Star Colored.png" width="30px">
+                    <img @click="ratingStar(3)" src="../assets/Star Colored.png" width="30px">
+                    <img @click="ratingStar(4)" src="../assets/Star.png" width="30px">
+                    <img @click="ratingStar(5)" src="../assets/Star.png" width="30px">
+                </div>
+                <div v-if="newRating == 4" class="col-sm-9 pl-0" style="cursor: pointer">
+                    <img @click="ratingStar(1)" src="../assets/Star Colored.png" width="30px">
+                    <img @click="ratingStar(2)" src="../assets/Star Colored.png" width="30px">
+                    <img @click="ratingStar(3)" src="../assets/Star Colored.png" width="30px">
+                    <img @click="ratingStar(4)" src="../assets/Star Colored.png" width="30px">
+                    <img @click="ratingStar(5)" src="../assets/Star.png" width="30px">
+                </div>
+                <div v-if="newRating == 5" class="col-sm-9 pl-0" style="cursor: pointer">
+                    <img @click="ratingStar(1)" src="../assets/Star Colored.png" width="30px">
+                    <img @click="ratingStar(2)" src="../assets/Star Colored.png" width="30px">
+                    <img @click="ratingStar(3)" src="../assets/Star Colored.png" width="30px">
+                    <img @click="ratingStar(4)" src="../assets/Star Colored.png" width="30px">
+                    <img @click="ratingStar(5)" src="../assets/Star Colored.png" width="30px">
+                </div>
             <div class="col-sm-2">
                 <button id="cancelBtn" class="mt-0 px-1 mr-2 py-1">Cancelar</button>
                 <button id="commentBtn" class="mt-0 px-2 py-1 mb-2">Comentar</button>
@@ -32,22 +68,30 @@
     </div>
 </template>
 <script>
-export default {
-  name: "AddComment",
-  data: () => ({
-      loggedUser: {},
-  }),
-  props:{
-    restaurant:{
-      type: Object,
-      required: true
-    }
-  },
-  created: function(){
-      this.loggedUser = this.$store.getters.getLoggedUser
-  }
+    export default {
+        name: "AddComment",
+        data: () => ({
+            loggedUser: {},
+            newComment: "",
+            newRating: "",
+        }),
+        props: {
+            restaurant: {
+                type: Object,
+                required: true
+            }
+        },
 
-}
+        created: function () {
+            this.loggedUser = this.$store.getters.getLoggedUser
+        },
+
+        methods: {
+            ratingStar(rating) {
+                this.newRating = rating
+            }
+        }
+    }
 </script>
 
 <style scoped>
