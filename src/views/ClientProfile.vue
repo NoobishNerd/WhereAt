711e5ee5a4f04b2b9e8ec5101728f7dcc6720515
ZeBitzz <9180270@esmad.ipp.pt>
2020-01-30 14:03:03 +0000

    Mudança visual do prefil do client para pipi

--- src/views/ClientProfile.vue
+++ src/views/ClientProfile.vue
@@ -1,75 +1,83 @@
 <template>
-  <div class="container">
-    <br>
-    <br>
-    <div class="row d-flex justify-content-sm-center">
-      <div class="col-sm-12">
-        <div class="row" style="border:1px solid black; border-radius: 6px;">
-          <div class="col-sm-3 text-center">
-            <br />
-            <br />
-            <img :src=user.profilePic class="rounded-circle" width="95px" height="95px" />
-            <br />
-            <h5 class="pt-2" id="brownBoldText">{{user.username}}</h5>
-            <button @click="changeUserImg" id="smallerButton" class="mt-2 py-2 px-3">Alterar Foto de Perfil</button>
-          </div>
-
-          <div class="col-sm-9">
-            <div class="row justify-content-sm-around">
-              <div @click="call('info')" class="col-sm-4 " style="border-bottom:1px solid black; border-left:1px solid black; border-radius: 0px 0px 0px 6px; cursor:pointer;">
-                <div class="row">
-                  <div class="col-sm-2 pt-1">
-                    <img src="../assets/main_user.png" width="40px"> 
-                  </div>
-                  <div class="col-sm-10 pt-3">
-                    <h5 class="text-center" id="brownText">Informações Pessoais</h5>                    
-                  </div>
-                </div>
-              </div>
-              <div @click="call('history')" class="col-sm-4" style="border-bottom:1px solid black; border-left:1px solid black; cursor: pointer;">
-                <div class="row">
-                  <div class="col-sm-2 pt-2">
-                    <img src="../assets/Reservations Icon.png" width="42px"> 
-                  </div>
-                  <div class="col-sm-10 pt-3">
-                    <h5 class="text-center" id="brownText">As Minhas Reservas</h5>                    
-                  </div>
-                </div>
-              </div>
-              <div @click="logout()" class="col-sm-4" style="border-bottom:1px solid black; border-left:1px solid black; cursor: pointer;">
-                <div class="row">
-                  <div class="col-sm-2 pt-2">
-                    <img src="../assets/Logout Icon.png" width="39px">  
-                  </div>
-                  <div class="col-sm-10 pt-3">
-                    <h5 class="text-center" id="brownText">Terminar Sessão</h5>                    
-                  </div>
-                </div>
-              </div>
-            </div>
-            <ClientInfo :user="user" v-if="component == 'info'"> </ClientInfo>
-            <input @click="getAdminAuth" v-if="component == 'history' && user.admin == true" type="button" value="Autorizar Restaurants">
-            <ClientHistory :id="Number(user.id)" v-if="component == 'history'"> </ClientHistory>
-          </div>
+  <div class="client">
+    <div class="d-flex justify-content-end">
+      <div @click="call('info')" style="cursor:pointer;">
+        <img
+          src="../assets/main_user.png"
+          height="20px"
+          style="vertical-align: middle; margin-right: 5px;"
+        />
+        <div class="text" id="brownText">Informações Pessoais</div>
+      </div>
+      <div @click="call('history')" style="cursor: pointer;">
+        <img
+          src="../assets/Reservations Icon.png"
+          height="30px"
+          style="vertical-align: middle; margin-right: 5px;"
+        />
+        <div class="text" id="brownText">As Minhas Reservas</div>
+      </div>
+      <div @click="logout()" style="cursor: pointer;">
+        <img
+          src="../assets/Logout Icon.png"
+          height="20px"
+          style="vertical-align: middle; margin-right: 5px;"
+        />
+        <div class="text" id="brownText">
+          Terminar Sessão
         </div>
+      </div>
+    </div>
+    <div class="row d-flex justify-content-sm-center flex-wrap pt-3">
+      <div class="col-sm-3">
+        <div class="text-center">
+          <img
+            :src="user.profilePic"
+            class="rounded-circle"
+            width="95px"
+            height="95px"
+          />
+          <br />
+          <h5 class="pt-2" id="brownBoldText">{{ user.username }}</h5>
+          <button
+            @click="changeUserImg"
+            id="smallerButton"
+            class="mt-2 py-2 px-3"
+          >
+            Alterar Foto de Perfil
+          </button>
+        </div>
+      </div>
+      <div class="col-sm-9">
+        <ClientInfo :user="user" v-if="component == 'info'"> </ClientInfo>
+        <input
+          @click="getAdminAuth"
+          v-if="component == 'history' && user.admin == true"
+          type="button"
+          value="Autorizar Restaurants"
+        />
+        <ClientHistory :id="Number(user.id)" v-if="component == 'history'">
+        </ClientHistory>
       </div>
     </div>
   </div>
 </template>
+
 <script>
 import ClientInfo from "@/components/ClientInfo.vue";
 import ClientHistory from "@/components/ClientHistory.vue";
+
 export default {
   name: "profileClient",
   data: () => ({
     component: "info",
     user: {
       id: "",
-      username:"",
-      profilePic:"",
-      email:"",
-      phone:"",
-      admin:""
+      username: "",
+      profilePic: "",
+      email: "",
+      phone: "",
+      admin: ""
     }
   }),
 
@@ -85,27 +93,26 @@ export default {
       this.component = newComponent;
     },
 
-    logout() {      
-      this.$store.commit("LOGOUT")
-      localStorage.setItem("loggedUser", "")
-      this.$router.replace("/")
+    logout() {
+      this.$store.commit("LOGOUT");
+      localStorage.setItem("loggedUser", "");
+      this.$router.replace("/");
     },
 
-    getAdminAuth(){
-      this.$router.push("/adminAuth")
+    getAdminAuth() {
+      this.$router.push("/adminAuth");
     },
 
-    changeUserImg(){
-      let newUserImg = prompt("Link da imagem:")
-      if (newUserImg != ""){
-        this.user.profilePic = newUserImg
-        this.$store.commit("CHANGE_USER_IMG",{
+    changeUserImg() {
+      let newUserImg = prompt("Link da imagem:");
+      if (newUserImg != "") {
+        this.user.profilePic = newUserImg;
+        this.$store.commit("CHANGE_USER_IMG", {
           id: this.user.id,
           profilePic: this.user.profilePic
-        })
-      }
-      else{
-        alert("Coloque o link da imagem!")
+        });
+      } else {
+        alert("Coloque o link da imagem!");
       }
     }
   },
@@ -116,20 +123,31 @@ export default {
 };
 </script>
 
-<style>
-#brownBoldText{
-  color: #F17526;
+<style scoped>
+.text {
+  margin-right: 15px;
+  vertical-align: middle;
+  display: inline;
+}
+
+.text-center {
+  vertical-align: middle !important;
+}
+
+#brownBoldText {
+  color: #f17526;
   font-family: "Raleway";
   font-weight: bold;
 }
 
-#brownText{
-  color: #F17526;
+#brownText {
+  color: #f17526;
+  font-size: 20px;
   font-family: "Raleway";
 }
 
 #smallerButton {
-  background-color: #F17526;
+  background-color: #f17526;
   border: none;
   color: white;
   text-align: center;
@@ -146,5 +164,4 @@ export default {
   -o-transition: all 0.3s ease-in-out;
   transition: all 0.3s ease-in-out;
 }
-
 </style>
