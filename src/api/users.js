3636fe5a4cac09d6ece47df3b3f25714443791cd
José Pedro <9180270@esmad.ipp.pt>
2020-06-14 18:42:08 +0100

    damn thats nice

--- src/api/users.js
+++ src/api/users.js
@@ -18,11 +18,13 @@ const users = {
       method: "POST",
       body: JSON.stringify(data),
       headers: {
-        "Content-Type": "application/json"
-      }
+        "Content-Type": "application/json",
+      },
     });
 
     if (response.ok) {
+      const responseData = await response.json();
+      alert(responseData);
       return response.json();
     } else {
       throw Error(response);
@@ -69,11 +71,13 @@ const users = {
       method: "POST",
       body: JSON.stringify(data),
       headers: {
-        "Content-Type": "application/json"
-      }
+        "Content-Type": "application/json",
+      },
     });
 
     if (response.ok) {
+      const responseData = await response.json();
+      alert(responseData);
       return response.json();
     } else {
       throw Error(response);
