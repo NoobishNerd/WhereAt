80b4f55ffeb1a3ca5b435dfce31266cb30e74b78
José Pedro <9180270@esmad.ipp.pt>
2020-06-14 17:30:33 +0100

    Mundo el savior

--- src/api/users.js
+++ src/api/users.js
@@ -1,110 +1,107 @@
-import API_URL from './config.js'
+import API_URL from "./config.js";
 
 const users = {
-    
-    async getUser(data) {
-        const response = await fetch(`${API_URL}utilizadores`, {
-            method: "GET",
-            body: JSON.stringify(data)
-        })
+  async getUser(data) {
+    const response = await fetch(`${API_URL}utilizadores`, {
+      method: "GET",
+      body: JSON.stringify(data),
+    });
 
-        if (response.ok) {
-            return response.json()
-        } else {
-            throw Error(response)
-        }
-    },
-    async registerUser(data) {
-        let user_name = data.user_name;
-        let email =data.email;
-        let password =data.password;
-        alert(JSON.stringify({user_name,email,password}))
-        const response = await fetch(`${API_URL}utilizadores`, {
-            method: "POST",
-            body: JSON.stringify({user_name,email,password})
-        })
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw Error(response);
+    }
+  },
+  async registerUser(data) {
+    const response = await fetch(`${API_URL}utilizadores`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
 
-        if (response.ok) {
-            
-            return response.json()
-        } else {
-            throw Error(response)
-        }
-    },
-    async updateUser(data, id) {
-        const response = await fetch(`${API_URL}utilizadores/${id}`, {
-            method: "PUT",
-            body: JSON.stringify(data)
-        })
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw Error(response);
+    }
+  },
+  async updateUser(data, id) {
+    const response = await fetch(`${API_URL}utilizadores/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(data),
+    });
 
-        if (response.ok) {
-            return response.json()
-        } else {
-            throw Error(response)
-        }
-    },
-    async deleteUser(id) {
-        const response = await fetch(`${API_URL}utilizadores/${id}`, {
-            method: "DELETE"
-        })
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw Error(response);
+    }
+  },
+  async deleteUser(id) {
+    const response = await fetch(`${API_URL}utilizadores/${id}`, {
+      method: "DELETE",
+    });
 
-        if (response.ok) {
-            return response.json()
-        } else {
-            throw Error(response)
-        }
-    },
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw Error(response);
+    }
+  },
 
+  async getRestaurant(data) {
+    const response = await fetch(`${API_URL}restaurantes`, {
+      method: "GET",
+      body: JSON.stringify(data),
+    });
 
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw Error(response);
+    }
+  },
+  async registerRestaurant(data) {
+    const response = await fetch(`${API_URL}restaurantes`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
 
-    async getRestaurant(data) {
-        const response = await fetch(`${API_URL}restaurantes`, {
-            method: "GET",
-            body: JSON.stringify(data)
-        })
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw Error(response);
+    }
+  },
+  async updateRestaurant(data, id) {
+    const response = await fetch(`${API_URL}restaurantes/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(data),
+    });
 
-        if (response.ok) {
-            return response.json()
-        } else {
-            throw Error(response)
-        }
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw Error(response);
+    }
+  },
+  async deleteRestaurant(id) {
+    const response = await fetch(`${API_URL}restaurantes/${id}`, {
+      method: "DELETE",
+    });
 
-    },
-    async registerRestaurant(data) {
-        const response = await fetch(`${API_URL}restaurantes`, {
-            method: "POST",
-            body: JSON.stringify(data)
-        })
-
-        if (response.ok) {
-            return response.json()
-        } else {
-            throw Error(response)
-        }
-    },
-    async updateRestaurant(data, id) {
-        const response = await fetch(`${API_URL}restaurantes/${id}`, {
-            method: "PUT",
-            body: JSON.stringify(data)
-        })
-
-        if (response.ok) {
-            return response.json()
-        } else {
-            throw Error(response)
-        }
-    },
-    async deleteRestaurant(id) {
-        const response = await fetch(`${API_URL}restaurantes/${id}`, {
-            method: "DELETE"
-        })
-
-        if (response.ok) {
-            return response.json()
-        } else {
-            throw Error(response)
-        }
-    },
-}
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw Error(response);
+    }
+  },
+};
 
 export default users;
