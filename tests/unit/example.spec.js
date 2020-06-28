1eb0d5e1631257d1ad3bf7dede4252e78789ecb5
José Pedro <9180270@esmad.ipp.pt>
2020-06-28 20:44:24 +0100

    trying to deploy again

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,11 +1,23 @@
-import { shallowMount } from "@vue/test-utils";
+import { createLocalVue, shallowMount, mount } from "@vue/test-utils";
 import NavBar from "@/components/NavBar.vue";
 import Home from "@/views/Home.vue";
 import ClientProfile from "@/views/ClientProfile.vue";
-
+import VueRouter from "vue-router"
 import Vue from 'vue'
 import Vuex from 'vuex'
+
 Vue.use(Vuex)
+
+
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+
+
+const routes= [{path: "/clientProfile/1" , component:ClientProfile}]
+
+const router = new VueRouter({
+  routes
+})
 
 let mock =  new Vuex.Store({
   state: {
@@ -38,31 +50,36 @@ describe("NAVBAR", () => {
     expect(wrapper.find("#main_userImg").isVisible()).toBe(true);
   });
   test("if logged in is true, main user image is visible", () => {
-    const wrapper = shallowMount(NavBar, {
+    const wrapper = mount(NavBar, {
       store: mock
     });
+    // eslint-disable-next-line no-console
+    console.log(wrapper)
     expect(wrapper.find("#loggedImg").src).toEqual("https://cdn.olhares.pt/client/files/foto/big/195/1956951.jpg");
   });
 });
 
-// describe("HOME PAGE", () => {
-//   test("if logged in is true, recommendations exist", () => {
-//     const wrapper = shallowMount(Home, {
-//       store: mock
-//     });
-//     expect(wrapper.find("#recommendationText").isVisible()).toBe(true);
-//   });
-//   test("restaurant with id 1 exists in home page", () => {
-//     const wrapper = shallowMount(Home, {
-//       store: mock
-//     });
-//     expect(wrapper.find("#restauranteN1").isVisible()).toBe(true);
-//   });
-// });
+describe("HOME PAGE", () => {
+  test("restaurant with id 1 exists in home page", () => {
+    const wrapper = mount(Home, {
+      store: mock
+    });
+    expect(wrapper.find("#restauranteN1").isVisible()).toBe(true);
+  });
+});
 
-// describe("PROFILE", () => {
-//   test("check if profile name is modelled after loggedUser", () => {
-//     const wrapper = shallowMount(ClientProfile, {store: mock});
-//     expect(wrapper.find("#nameTxt").value).toEqual("Sr.Alberto");
-//   });
-// });
+describe("PROFILE", () => {
+  test("check if profile name is modelled after loggedUser", () => {
+    jest.useFakeTimers()
+    const wrapper = mount(ClientProfile, {
+      store: mock,
+      localVue,
+      router
+    });
+    jest.advanceTimersByTime(3000)
+    // eslint-disable-next-line no-console
+    alert(wrapper.vm.$route)
+    expect(wrapper.vm.$route).toBeInstanceOf(Object)
+    expect(wrapper.vm.user.username).toBe("Sr.Alberto");
+  });
+});
